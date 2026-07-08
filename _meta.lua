233784a66810d17586b587c09a4db64d0514050b
DisastrousPatience83 <edsonmtejada@gmail.com>
2026-07-08 08:31:03 -0500

    Add files via upload

---
+++ _meta.lua
@@ -0,0 +1,7 @@
+local _ = require("gettext")
+
+return {
+	name = "floatingdictionary",
+	fullname = _("Floating Dictionary"),
+	description = _([[Shows a compact Floating Dictionary before opening the full dictionary popup.]]),
+}
