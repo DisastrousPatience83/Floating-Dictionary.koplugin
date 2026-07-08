5daad7d5d6aaba244134e9635cee5a2f6d70069f
DisastrousPatience83 <edsonmtejada@gmail.com>
2026-07-08 08:28:51 -0500

    _meta.lua

--- _meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-
-return {
-	name = "floatingdictionary",
-	fullname = _("Floating Dictionary"),
-	description = _([[Shows a compact Floating Dictionary before opening the full dictionary popup.]]),
-}
